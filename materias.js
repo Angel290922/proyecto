cd78b3e9d74280045b472439cb4b23bf72ba1d57
Angel290922 <144636720+Angel290922@users.noreply.github.com>
2024-09-23 17:07:42 -0600

    Add files via upload

---
+++ materias.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+materiasCallback({
+    "materias": [
+        {
+            "nombre": "Matemáticas",
+            "codigo": "MAT101",
+            "profesor": "Dr. García",
+            "creditos": 5,
+            "semestre": 1
+        },
+        {
+            "nombre": "Programación",
+            "codigo": "PRO102",
+            "profesor": "Ing. Fernández",
+            "creditos": 6,
+            "semestre": 2
+        }
+        /* Agrega otros 8 registros aquí */
+    ]
+});
